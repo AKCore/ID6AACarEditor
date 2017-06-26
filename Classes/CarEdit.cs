282ba1b2a531380bd37bff4b39a7cd0cf83dabcc
SanGawku <noemail@privacy.com>
2017-06-26 09:08:32 -0700

    First commit with solution

---
+++ Classes/CarEdit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ID6AACarEditor.Classes
+{
+    class CarEdit
+    {
+        private string[] PartsList;
+        private string[] CarIdentity;
+
+        public string[] getPartsList()
+        {
+            return PartsList;
+        }
+
+        public string[] getCarIdentity()
+        {
+            return CarIdentity;
+        }
+    }
+}
---
+++ Forms/Main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ID6AACarEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (About box = new About())
+            {
+                box.ShowDialog(this);
+            }
+
+        }
+
+        private void openCarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
